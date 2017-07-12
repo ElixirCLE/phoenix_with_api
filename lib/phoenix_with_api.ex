0ae428567c3c8fd793168d598cfc2912857266a7
Chris Rees <crees@covermymeds.com>
2017-07-11 21:58:52 -0400

    Add Heroku and Travis flags

--- lib/phoenix_with_api.ex
+++ lib/phoenix_with_api.ex
@@ -1,11 +1,14 @@
 defmodule PhoenixWithApi do
 
   @moduledoc File.read!(Path.join([__DIR__, "../README.md"]))
-  
+
   use MixTemplates,
     name:       :phoenix_with_api,
     short_desc: "Template for ....",
-    source_dir: "../template"
+    source_dir: "../template",
+    options: [
+      heroku: [ to: :is_heroku?, default: false ],
+      travis_ci: [ to: :is_travis?, default: false ]
+    ]
 
-  
 end
